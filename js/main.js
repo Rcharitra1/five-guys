093410cc7991c472eb368b9765ba3a1712e84553
Rcharitra1 <71350761+Rcharitra1@users.noreply.github.com>
2020-10-22 19:02:55 -0600

    reposive

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $(".overlay").hide();
+
+$(".menu-btn").click(function(e){
+  $(".menu-btn .fas").toggleClass("fa-bars fa-times-circle")
+  $("nav ul").toggle();
+  e.preventDefault();
+})
+
+$(".choose-locations, locations a").click(function(e){
+  $(".overlay").toggle();
+  e.preventDefault();
+})
+
+// $(".locations a").click(function(e){
+//   $(".overlay").toggle();
+//   e.preventDefault();
+// })
